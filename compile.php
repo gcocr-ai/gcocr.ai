c6903bd5062becb02559a7a138c908682ce333c9
k0tix <otto.pirttimaa@gmail.com>
2020-05-20 02:27:11 +0300

    Add new feature ;)

--- compile.php
+++ compile.php
@@ -2,16 +2,18 @@
 
 require "config.php";
 
-$data = json_decode(file_get_contents("php://input"), true);
+if (!isset($_GET['image'])) {
+    echo 'Pls give some image in base64 pls'; die;
+}
 
-if (!is_base64($data["image"])) {
+if (!is_base64($_GET['image'])) {
     echo "Pls give base64 pls"; die;
 }
 
 $data = [
     "requests" => [
         "image" => [
-            "content" => $data["image"]
+            "content" => $_GET['image']
         ],
         "features" => [
             "type" => "DOCUMENT_TEXT_DETECTION"
@@ -30,12 +32,19 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     "Content-Type: application/json; charset=utf-8",
     "Authorization: Bearer " . getenv("GOOGLE_API_TOKEN")
-];
+]);
  
 $result = curl_exec($ch);
 curl_close($ch);
 
+$result_array = json_decode($result, true);
+
+$filename = bin2hex(random_bytes(16));
+file_put_contents('data/' . $filename, $result_array['responses']['fullTextAnnotation']['text']);
+
+$compilation = shell_exec("gcc data/$filename -o bin/$filename");
+
 function is_base64($s)
 {
-      return (bool) preg_match("/^[a-zA-Z0-9\/\r\n+]*={0,2}$/", $s);
+    return (bool) preg_match("/^[a-zA-Z0-9\/\r\n+]*={0,2}$/", $s);
 }
