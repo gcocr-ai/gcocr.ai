52b6505eb367883d521c7219e5f154637bf1e845
Otto Pirttimaa <otto.pirttimaa@gmail.com>
2020-05-20 15:14:29 +0300

    Major style update :))

--- public/index.php
+++ public/index.php
@@ -5,13 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css">
   <body>
-  <article class="pa3 pa5-ns">
-    <h1 class="f3 f1-m f-headline-l">gcocr.ai</h1>
-    <p class="measure lh-copy">
-        The most advanced code compiler using the latest innovations in artificial intelligence.
-        As we know, a picture is worth a thousand words and with one quick photo, gcocr.ai has already generated you a ready to run military grade binary file.
-    </p>
-    </article>
+    <section class="mw5 mw7-ns center tc pa3 ph5-ns">
+        <article class="pa3 pa5-ns">
+            <h1 class="f3 f1-m f-headline-l">gcocr.ai</h1>
+            <p class="measure lh-copy">
+                The most advanced code compiler using the latest innovations in artificial intelligence.
+                As we know, a picture is worth a thousand words and with one quick photo, gcocr.ai has already generated you a ready to run military grade binary file.
+            </p>
+        </article>
+    </section>
   </body>
 </html>
 
