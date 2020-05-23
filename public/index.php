d6f3663474d7ab3c40e79bcddd9753349641d8b5
Otto Pirttimaa <otto.pirttimaa@gmail.com>
2020-05-23 11:42:38 +0300

    Style changes to main page and file input form

--- public/index.php
+++ public/index.php
@@ -8,13 +8,13 @@
     <section class="mw5 mw7-ns center tc pa3 ph5-ns">
         <article class="pa3 pa5-ns">
             <h1 class="f3 f1-m f-headline-l">gcocr.ai</h1>
-            <p class="measure lh-copy">
+            <p class="measure lh-copy f4 sans-serif">
                 The most advanced code compiler using the latest innovations in artificial intelligence.
                 As we know, a picture is worth a thousand words and with one quick photo, gcocr.ai has already generated you a ready to run military grade binary file.
             </p>
         </article>
     </section>
-    <p align="center"><iframe allowtransparency="true" style="background: #FFFFFF; width:60%; height:500px;" src="https://recyclr.pro/upload.php" title="" id="aaa"></iframe></p>
+    <p align="center"><iframe allowtransparency="true" style="background: #FFFFFF; width:60%; height:500px;" src="upload.php" title="" id="aaa"></iframe></p>
   </body>
 </html>
 
--- public/upload.php
+++ public/upload.php
@@ -3,18 +3,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css">
     <body>
-    <main class="pa4 black-80">
+    <main class="pa4 black-80 sans-serif">
         <form class="measure center" action="indexkax.php" method="post" enctype="multipart/form-data">
-            <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+            <fieldset class="ba b--transparent ph0 mh0">
                 <legend class="f4 fw6 ph0 mh0">Select image to upload:</legend>
                 <div class="mt3">
                     <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*;capture=camera" onchange=""> <br />
-
                 </div>
-                <legend class="f4 fw6 ph0 mh0">Use advanced post-processing</legend>
-                <div class="mt3">
-                    <input type="checkbox" id="postP" name="postP" checked> <br />
-                </div>
+                <br/>
+                <label class="pa0 ma0 lh-copy f5 pointer"><input type="checkbox" id="postP" name="postP" checked> Use advanced post-processing</label>        
             </fieldset>
             <div class="">
                 <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Upload Image" name="submit" onClick="document.getElementById('load').style.display = '';return true;">
